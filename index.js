4895b2aaa1a6a8edf865b14bd73107c4336901f9
Bianca <biancancanal@gmail.com>
2021-03-08 15:46:13 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+const name = [""]
+const kindGift =[""]
+
+const newArray = [];
+
+function writeCards(name, kindGift) {
+  for (let i = 0; i < name.length; i++) {
+    newArray.push(`Thank you, ${name[i]}, for the wonderful ${kindGift} gift!`);
+  }
+  return newArray;
+}
+
+
+function countDown() {
+    let countup = 0;
+    while (countup <= 10) {
+      console.log(countup++);
+    } 
+    return countup;
+}
+
+
+
